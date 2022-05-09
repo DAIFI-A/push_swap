f33f7670d9c952c2f202dca8eaa6b8e066baf718
Daifi-Abderrahmane <74793035+DAIFI-A@users.noreply.github.com>
2022-05-10 05:51:50 +0800

    Update sort_five.c

--- sort_five.c
+++ sort_five.c
@@ -10,15 +10,32 @@ int push_first_2_elements(t_stack **a, t_stack **b, int i)
 	return (i);
 }
 
+int smallest_num(t_stack *stack)
+{
+    int     min;
+
+    min = stack->num;
+    while (stack->next)
+    {
+        if (stack->num < min)
+            min = stack->num;
+        stack = stack->next;
+    }
+    if(stack->num < min)
+        min = stack->num;
+    return (min);
+}
+
 int sort_five(t_stack **val , t_stack **val1, int i)
 {
+    int min;
+
+    
     //first we need to push the top 2 elements to stack_b
     i += push_first_2_elements(val, val1, i);
     //then we deal with the rest by the three sort algo
     i += sort_3(val);
-    if ((*val1)->num < (*val1)->next)
-    {
-        i += sa_b(val1);
-        write(1, "sb\n", 3);
-    }
+    min = smallest_num(val);
+    if ()
+    i += pa_b()
 }
